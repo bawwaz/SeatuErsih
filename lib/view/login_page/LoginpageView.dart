2cec9c68f21a8948b74dcd8b36f280c396c7f86c
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-05-02 10:19:57 +0700

    style: [DNA] add page register

---
+++ lib/view/login_page/LoginpageView.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(28),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 58, left: 145),
+                child: Image.asset("assets/img/icon-app.png"),
+              ),
+          
+              SizedBox(
+                height: 29,
+              ),
+          
+              Row(
+                children: [
+                  Text(
+                    "Login ",
+                    style: GoogleFonts.poppins(
+                      color: Color(0xFF000000),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                    ),
+                  ),
+                  Text(
+                    "SEATUERSIH",
+                    style: GoogleFonts.poppins(
+                      color: Color(0xFF7EC1EB),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                    ),
+                  ),
+                ],
+              ),
+          
+              SizedBox(
+                height: 20,
+              ),
+
+              SizedBox(
+                width: double.infinity,
+                height: 53,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: "Name",
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Color(0xFF8A8A8A), // atur warna ikon
+                    ),
+                    labelStyle: TextStyle(
+                      // color: Color(0xFF8A8A8A), // atur warna teks
+                    ),
+                  ),
+                  style: TextStyle(
+                  ),
+                ),
+              ),
+
+
+              SizedBox(
+                height: 20,
+              ),
+
+              SizedBox(
+                width: double.infinity,
+                height: 53,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.lock,
+                    color: Color(0xFF8A8A8A),),
+                  ),
+                ),
+              ),
+
+              // SizedBox(
+              //   height: 10,
+              // ),
+          
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(onPressed: () {},
+                    child: Text("Forget Password?",
+                      style: GoogleFonts.poppins(
+                        color: Color(0xFF7EC1EB),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 12,
+                      ),)),
+              ),
+          
+              SizedBox(
+                height: 20,
+              ),
+          
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF7EC1EB),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15))),
+                      child: Text(
+                        "Sign In",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )),
+                ),
+              ),
+          
+              SizedBox(
+                height: 20,
+              ),
+          
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: 99,
+                      height: 1,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF8A8A8A),
+                      ),
+                    ),
+                  ),
+          
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      "or continue with",
+                      style: GoogleFonts.poppins(
+                        color: Color(0xFF8A8A8A),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+          
+                  Expanded(
+                    child: Container(
+                      width: 99,
+                      height: 1,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF8A8A8A),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 33,
+              ),
+
+              SizedBox(
+                  width: double.infinity,
+                  height: 53,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFFFFFF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      elevation: 5, // Menentukan elevasi bayangan
+                      shadowColor: Colors.black, // Menentukan warna bayangan
+
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset("assets/img/google-icon.png"),
+                        Text(
+                          "Sign In with Google",
+                          style: GoogleFonts.poppins(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                        Container(),
+                      ],
+                    ),
+                  )
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don’t have an account?",
+                    style: GoogleFonts.poppins(
+                      color: Color(0xFF1F1F1F),
+                      fontWeight: FontWeight.normal,
+                      fontSize: 13,
+                    ),
+                  ),
+
+                  TextButton(onPressed: () {},
+                      child: Text("Sign Up",
+                        style: GoogleFonts.poppins(
+                          color: Color(0xFF7EC1EB),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 13,
+                        ),)),
+                ],
+              ),
+          
+          
+          
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/register_page/RegisterviewPage.dart
+++ lib/view/register_page/RegisterviewPage.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:seatu_ersih/view/register_page/register_page.controller.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class Registerpage extends StatelessWidget {
+  const Registerpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(RegisterPageController());
     return Scaffold(
       body: Container(
         margin: EdgeInsets.all(28),
@@ -17,32 +21,24 @@ class LoginPage extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 58, left: 145),
                 child: Image.asset("assets/img/icon-app.png"),
               ),
-          
+
               SizedBox(
                 height: 29,
               ),
-          
+
               Row(
                 children: [
                   Text(
-                    "Login ",
+                    "Register",
                     style: GoogleFonts.poppins(
                       color: Color(0xFF000000),
                       fontWeight: FontWeight.bold,
                       fontSize: 28,
                     ),
                   ),
-                  Text(
-                    "SEATUERSIH",
-                    style: GoogleFonts.poppins(
-                      color: Color(0xFF7EC1EB),
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28,
-                    ),
-                  ),
                 ],
               ),
-          
+
               SizedBox(
                 height: 20,
               ),
@@ -57,7 +53,7 @@ class LoginPage extends StatelessWidget {
                     ),
                     labelText: "Name",
                     prefixIcon: Icon(
-                      Icons.account_circle,
+                      Icons.person,
                       color: Color(0xFF8A8A8A), // atur warna ikon
                     ),
                     labelStyle: TextStyle(
@@ -82,32 +78,74 @@ class LoginPage extends StatelessWidget {
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(15),
                     ),
-                    labelText: "Password",
-                    prefixIcon: Icon(Icons.lock,
-                    color: Color(0xFF8A8A8A),),
+                    labelText: "Email",
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Color(0xFF8A8A8A), // atur warna ikon
+                    ),
+                    labelStyle: TextStyle(
+                      // color: Color(0xFF8A8A8A), // atur warna teks
+                    ),
+                  ),
+                  style: TextStyle(
                   ),
                 ),
               ),
 
-              // SizedBox(
-              //   height: 10,
-              // ),
-          
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(onPressed: () {},
-                    child: Text("Forget Password?",
-                      style: GoogleFonts.poppins(
-                        color: Color(0xFF7EC1EB),
-                        fontWeight: FontWeight.w600,
-                        fontSize: 12,
-                      ),)),
-              ),
-          
               SizedBox(
                 height: 20,
               ),
-          
+
+              SizedBox(
+                width: double.infinity,
+                height: 53,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.lock,
+                      color: Color(0xFF8A8A8A),),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 17,
+              ),
+
+              Row(
+                children: [
+                  Obx(() => GestureDetector(
+                    onTap: () => controller.checked.value = !controller.checked.value,
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Color(0xFFC1C1C1)),
+                          borderRadius: BorderRadius.circular(5),
+                        color: controller.checked.value ? Color(0xFF7EC1EB) : Colors.transparent
+                      ),
+                      child: controller.checked.value ? Icon(Icons.check, size: 18, color: Colors.white,) : SizedBox(height: 20, width: 20,),
+                    ),
+                  )),
+                  Container(
+                    width: 300,
+                    margin: EdgeInsets.only(left: 8),
+                    child: Text("I agree with SEATUERSIH Terms of Service, Privacy Policy. and default Notification Settings.",
+                      style: GoogleFonts.poppins(
+                        color: Color(0xFF1F1F1F),
+                        fontWeight: FontWeight.w500,
+                        fontSize: 10,
+                      ),),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 49,
+              ),
+
               Center(
                 child: SizedBox(
                   width: double.infinity,
@@ -120,7 +158,7 @@ class LoginPage extends StatelessWidget {
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(15))),
                       child: Text(
-                        "Sign In",
+                        "Create account",
                         style: GoogleFonts.poppins(
                           color: Colors.white,
                           fontWeight: FontWeight.w600,
@@ -128,11 +166,11 @@ class LoginPage extends StatelessWidget {
                       )),
                 ),
               ),
-          
+
               SizedBox(
                 height: 20,
               ),
-          
+
               Row(
                 children: [
                   Expanded(
@@ -144,7 +182,7 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-          
+
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10),
                     child: Text(
@@ -156,7 +194,7 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-          
+
                   Expanded(
                     child: Container(
                       width: 99,
@@ -192,7 +230,7 @@ class LoginPage extends StatelessWidget {
                       children: [
                         Image.asset("assets/img/google-icon.png"),
                         Text(
-                          "Sign In with Google",
+                          "Sign Up with Google",
                           style: GoogleFonts.poppins(
                             color: Colors.black,
                             fontWeight: FontWeight.w600,
@@ -213,27 +251,26 @@ class LoginPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Don’t have an account?",
+                    "Already have an account?",
                     style: GoogleFonts.poppins(
                       color: Color(0xFF1F1F1F),
-                      fontWeight: FontWeight.w600,
+                      fontWeight: FontWeight.normal,
                       fontSize: 13,
                     ),
                   ),
 
-                  TextButton(onPressed: () {
-                  },
+                  TextButton(onPressed: () {},
                       child: Text("Sign In",
                         style: GoogleFonts.poppins(
                           color: Color(0xFF7EC1EB),
-                          fontWeight: FontWeight.w600,
+                          fontWeight: FontWeight.normal,
                           fontSize: 13,
                         ),)),
                 ],
               ),
-          
-          
-          
+
+
+
             ],
           ),
         ),
