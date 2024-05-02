a7503e417f0e2b7af57df3e2240849a88cd4881a
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-05-02 08:58:22 +0700

    style [DNA] add page ui login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:seatu_ersih/view/HomepageView.dart';
+import 'package:seatu_ersih/view/LoginpageView.dart';
 import 'widget/Homepage/HomepageBar.dart';
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LoginPage(),
     );
   }
 }
