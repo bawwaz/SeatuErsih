8e3102ff536d185f52077ddca4043513ab5428c4
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-05-03 09:13:31 +0700

    style: [DNA] add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:seatu_ersih/view/HomepageView.dart';
 import 'package:seatu_ersih/view/login_page/LoginpageView.dart';
 import 'package:seatu_ersih/view/register_page/RegisterviewPage.dart';
+import 'package:seatu_ersih/view/splash_screen/splash_screen_view.dart';
 import 'widget/Homepage/HomepageBar.dart';
 
 void main() {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: SplashScreenPage(),
     );
   }
 }
