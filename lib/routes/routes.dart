f3572502e250dbba0e078fffd480ea24ae4358ca
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-05-27 11:37:11 +0700

    style: [DNA] add ui order booking regular

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:seatu_ersih/view/HomepageView.dart';
 import 'package:seatu_ersih/view/login_page/LoginpageView.dart';
 import 'package:seatu_ersih/view/order_booking_deep/order_booking_deep_view.dart';
+import 'package:seatu_ersih/view/order_booking_regular/order_booking_regular_view.dart';
 import 'package:seatu_ersih/view/profile_page/profileView.dart';
 import 'package:seatu_ersih/view/register_page/RegisterviewPage.dart';
 
@@ -11,6 +12,7 @@ class Routes {
   static String regis = '/regis';
   static String profile = '/profile';
   static String orderbookingdeep = '/orderdeep';
+  static String orderbookingregular = '/orderregular';
 
   static List<GetPage> pages = [
     GetPage(name: home, page: () => HomePage()),
@@ -18,5 +20,6 @@ class Routes {
     GetPage(name: regis, page: () => RegisterPage()),
     GetPage(name: profile, page: ()=> ProfilePage()),
     GetPage(name: orderbookingdeep, page: ()=> OrderBookingDeepView()),
+    GetPage(name: orderbookingregular, page: ()=> OrderBookingRegularView()),
   ];
 }
