56c7007941304302a43baa0b4046a903add1202d
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-06-07 16:41:48 +0700

    feat: [DNA] added enter shoe deep page

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      initialRoute: Routes.REG_CLEAN_LIST,
+      initialRoute: Routes.DEEP_CLEAN_LIST,
       getPages: AppPages.routes,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
