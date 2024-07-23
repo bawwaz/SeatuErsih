e913510db672d98ed9e3350518334f2306bf9206
dudedod67 <122068251+dudedod67@users.noreply.github.com>
2024-07-23 08:34:52 +0700

    feat: [DNA] fetch otp

--- lib/app/pages/intial_pages/otp_page/otp_page_controller.dart
+++ lib/app/pages/intial_pages/otp_page/otp_page_controller.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:seatu_ersih/app/router/app_pages.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OtpPageController extends GetxController {
-   var focusNodes = List.generate(4, (_) => FocusNode()).obs;
-  var controllers = List.generate(4, (_) => TextEditingController()).obs;
-  var isFocused = List.generate(4, (_) => false.obs);
+  var focusNodes = List.generate(6, (_) => FocusNode()).obs;
+  var controllers = List.generate(6, (_) => TextEditingController()).obs;
+  var isFocused = List.generate(6, (_) => false.obs);
 
   @override
   void onClose() {
@@ -19,5 +22,40 @@ class OtpPageController extends GetxController {
 
   void updateFocus(int index, bool value) {
     isFocused[index].value = value;
+  }
+
+  Future<void> verifyOtp(String pinCode) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+
+    var headers = {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    var data = {
+      'otp': pinCode,
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse("{{url}}/users/verify-otp"),
+        headers: headers,
+        body: data,
+      );
+
+      if (response.statusCode == 200) {
+        Get.snackbar("Berhasil", "Kode OTP berhasil diverifikasi",
+            snackPosition: SnackPosition.TOP, backgroundColor: Colors.green);
+        Get.offAllNamed(Routes.BTMNAVBAR);
+      } else {
+        Get.snackbar(
+            "Gagal ${response.statusCode}", "Kode OTP gagal diverifikasi",
+            snackPosition: SnackPosition.TOP, backgroundColor: Colors.red);
+      }
+    } catch (e) {
+      Get.snackbar("Gagal", e.toString(),
+          snackPosition: SnackPosition.TOP, backgroundColor: Colors.red);
+      print(e);
+    }
   }
 }
--- lib/app/pages/intial_pages/otp_page/widget/otp_textfields.dart
+++ lib/app/pages/intial_pages/otp_page/widget/otp_textfields.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:flutter/services.dart';
-import 'package:seatu_ersih/themes/theme.dart';
+import 'package:seatu_ersih/app/pages/intial_pages/otp_page/otp_page_controller.dart';
 
 class OtpTextfields extends StatelessWidget {
-  const OtpTextfields({super.key});
+  final OtpPageController controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Container(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: List.generate(6, (index) {
+        return Container(
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)),
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.grey),
+          ),
           child: SizedBox(
             height: 45,
-            width: MediaQuery.sizeOf(context).width / 7,
+            width: MediaQuery.of(context).size.width / 7,
             child: TextField(
+              controller: controller.controllers[index],
+              focusNode: controller.focusNodes[index],
               onChanged: (value) {
-                if (value.length == 1) {
+                if (value.length == 1 && index < 5) {
                   FocusScope.of(context).nextFocus();
+                } else if (value.isEmpty && index > 0) {
+                  FocusScope.of(context).previousFocus();
                 }
               },
               style: Theme.of(context).textTheme.headlineLarge,
@@ -37,148 +43,8 @@ class OtpTextfields extends StatelessWidget {
               ],
             ),
           ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)),
-          child: SizedBox(
-            height: 45,
-            width: MediaQuery.sizeOf(context).width / 7,
-            child: TextField(
-              onChanged: (value) {
-                if (value.length == 1) {
-                  FocusScope.of(context).nextFocus();
-                }
-              },
-              style: Theme.of(context).textTheme.headlineLarge,
-              decoration: InputDecoration(
-                border: InputBorder.none, // Remove default border
-                enabledBorder: InputBorder.none, // Remove enabled border
-                focusedBorder: InputBorder.none, // Remove focused border
-              ),
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              inputFormatters: [
-                LengthLimitingTextInputFormatter(1),
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)),
-          child: SizedBox(
-            height: 45,
-            width: MediaQuery.sizeOf(context).width / 7,
-            child: TextField(
-              onChanged: (value) {
-                if (value.length == 1) {
-                  FocusScope.of(context).nextFocus();
-                }
-              },
-              style: Theme.of(context).textTheme.headlineLarge,
-              decoration: InputDecoration(
-                border: InputBorder.none, // Remove default border
-                enabledBorder: InputBorder.none, // Remove enabled border
-                focusedBorder: InputBorder.none, // Remove focused border
-              ),
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              inputFormatters: [
-                LengthLimitingTextInputFormatter(1),
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)),
-          child: SizedBox(
-            height: 45,
-            width: MediaQuery.sizeOf(context).width / 7,
-            child: TextField(
-              onChanged: (value) {
-                if (value.length == 1) {
-                  FocusScope.of(context).nextFocus();
-                }
-              },
-              style: Theme.of(context).textTheme.headlineLarge,
-              decoration: InputDecoration(
-                border: InputBorder.none, // Remove default border
-                enabledBorder: InputBorder.none, // Remove enabled border
-                focusedBorder: InputBorder.none, // Remove focused border
-              ),
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              inputFormatters: [
-                LengthLimitingTextInputFormatter(1),
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)),
-          child: SizedBox(
-            height: 45,
-            width: MediaQuery.sizeOf(context).width / 7,
-            child: TextField(
-              onChanged: (value) {
-                if (value.length == 1) {
-                  FocusScope.of(context).nextFocus();
-                }
-              },
-              style: Theme.of(context).textTheme.headlineLarge,
-              decoration: InputDecoration(
-                border: InputBorder.none, // Remove default border
-                enabledBorder: InputBorder.none, // Remove enabled border
-                focusedBorder: InputBorder.none, // Remove focused border
-              ),
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              inputFormatters: [
-                LengthLimitingTextInputFormatter(1),
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(color: Colors.grey)),
-          child: SizedBox(
-            height: 45,
-            width: MediaQuery.sizeOf(context).width / 7,
-            child: TextField(
-              onChanged: (value) {
-                if (value.length == 1) {
-                  FocusScope.of(context).nextFocus();
-                }
-              },
-              style: Theme.of(context).textTheme.headlineLarge,
-              decoration: InputDecoration(
-                border: InputBorder.none, // Remove default border
-                enabledBorder: InputBorder.none, // Remove enabled border
-                focusedBorder: InputBorder.none, // Remove focused border
-              ),
-              keyboardType: TextInputType.number,
-              textAlign: TextAlign.center,
-              inputFormatters: [
-                LengthLimitingTextInputFormatter(1),
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-            ),
-          ),
-        ),
-      ],
+        );
+      }),
     );
   }
 }
